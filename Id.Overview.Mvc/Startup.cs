c03e0c885cbc1c47d49e64bc92487b4db2be6378
Diniz Rezende <cdinizrezend@dxc.com>
2020-03-19 17:42:54 -0300

    Add project files.

---
+++ Id.Overview.Mvc/Startup.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Id.Overview.Mvc.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Id.Overview.Mvc
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+                //Lockout
+                options.Lockout.AllowedForNewUsers = true;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(1);
+                options.Lockout.MaxFailedAccessAttempts = 1;
+
+                //Password
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireDigit = true;
+
+                //SignIn
+                options.SignIn.RequireConfirmedEmail = false;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+
+                //Tokens
+                //options.Tokens.AuthenticatorTokenProvider =
+                //options.Tokens.ChangeEmailTokenProvider = 
+                //options.Tokens.ChangePhoneNumberTokenProvider =
+                //options.Tokens.EmailConfirmationTokenProvider =
+                //options.Tokens.PasswordResetTokenProvider =
+
+                //User
+                //options.User.AllowedUserNameCharacters = "";
+                options.User.RequireUniqueEmail = false;
+            })
+             .AddEntityFrameworkStores<ApplicationDbContext>()
+             .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(options => {
+                //options.AccessDeniedPath = ""
+                //options.ClaimsIssuer = ""
+                //options.Cookie.Domain = ""
+                //options.Cookie.Expiration = 
+                options.Cookie.HttpOnly = true;
+                options.Cookie.Name = "AspNetCore.Cookies";
+                options.Cookie.SameSite = Microsoft.AspNetCore.Http.SameSiteMode.Lax;
+                options.Cookie.SecurePolicy = Microsoft.AspNetCore.Http.CookieSecurePolicy.SameAsRequest;
+                //options.CookieManager = 
+                //options.DataProtectionProvider = 
+                //options.EventsType = 
+                options.ExpireTimeSpan = TimeSpan.FromDays(14);
+                //options.LoginPath = "";
+                //options.LogoutPath = "";
+                //options.ReturnUrlParameter = "ReturnUrl";
+                //options.SessionStore = 
+                options.SlidingExpiration = true;
+                //options.TicketDataFormat = 
+
+            }
+            );
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
